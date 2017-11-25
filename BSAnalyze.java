b3e43628abc9973200fdf6db1496848519748863
Katherine Bigelow <kbigelow@berkeley.edu>
2017-11-24 23:21:33 -0800

    Final commit.

--- BSAnalyze.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Katherine on 11/10/2017.
- */
-public class BSAnalyze {
-    //will analyze biosafety levels of a composition
-    //reads in from data on what objects/etc correspond to what bs level
-}
--- BUAAnalyze.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Katherine on 11/10/2017.
- */
-public class BUAAnalyze {
-    //Quality Checks the BUA
-
-}
